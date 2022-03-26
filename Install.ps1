e15fd4ac95f5ba7d4c012a53f3b5a98bbefbbab0
Jleal019 <jleal019@fiu.edu>
2022-03-26 16:39:19 -0400

    Initial commit.

---
+++ Install.ps1
@@ -0,0 +1,90 @@
+﻿
+$currentUser = ((Get-WmiObject -ClassName Win32_ComputerSystem).Username).Split('\')[1]
+
+Copy-Item ".\Utilities Sites\*" "C:\Users\$currentUser\Desktop\"
+
+
+Write-Host "Installing 7Zip and Google Chrome..."
+
+$process = (Start-Process -FilePath 'Ninite 7Zip Chrome Installer.exe' -PassThru -Wait)
+
+$process.WaitForExit()
+
+if ($process.ExitCode -eq 0)
+    {
+        Write-Host "7Zip and Google Chrome installed successfully."
+    }
+else
+    {
+        Write-Host "7Zip and Google Chrome exited with code: " $process.ExitCode
+    }
+
+
+
+Write-Host "Installing Acrobat Reader..."
+
+$process = (Start-Process -FilePath readerdc64_en_xa_cra_install.exe -PassThru -Wait)
+
+$process.WaitForExit()
+
+if ($process.ExitCode -eq 0)
+    {
+        Write-Host "Acrobat Reader installed successfully."
+    }
+else
+    {
+        Write-Host "Acrobat Reader exited with code: " $process.ExitCode
+    }
+
+
+
+Write-Host "Installing TRaCS..."
+
+$process = (Start-Process -FilePath '.\Tracs\TraCS Florida 11.3.2.msi' -PassThru -Wait)
+
+$process.WaitForExit()
+
+if ($process.ExitCode -eq 0)
+    {
+        Write-Host "TRaCS installed successfully."
+    }
+else
+    {
+        Write-Host "TRaCS exited with code: " $process.ExitCode
+    }
+
+
+ 
+Write-Host "Installing Google Drive..."
+
+$process = (Start-Process -FilePath 'GoogleDriveSetup.exe' -PassThru -Wait)
+
+$process.WaitForExit()
+
+if ($process.ExitCode -eq 0)
+    {
+        Write-Host "Google Drive installed successfully."
+    }
+else
+    {
+        Write-Host "Google Drive exited with code: " $process.ExitCode
+    }
+
+
+
+Write-Host "Installing Mocha Mainframe..."
+
+$process = (Start-Process -FilePath '.\Miami Dade County CJIS MainFrame\tn3270.msi' -PassThru -Wait)
+
+$process.WaitForExit()
+
+if ($process.ExitCode -eq 0)
+    {
+        Write-Host "Mocha Mainframe installed successfully."
+    }
+else
+    {
+        Write-Host "Mocha Mainframe exited with code: " $process.ExitCode
+    }
+
+
---
+++ README.txt
@@ -0,0 +1,16 @@
+
+Open Powershell as an admin, change the directory to whatever the flash drive is and execute
+
+the following command.
+
+powershell.exe -ExecutionPolicy Bypass -File '.\Install.ps1'
+
+
+------------------------------------
+README
+------------------------------------
+All programs must stay within their respective directories.
+
+
+Adobe Acrobat must be set to read-only or it deletes itself.
+
